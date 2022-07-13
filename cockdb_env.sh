7b989ecb5ac56c9564dd48847e0bbd398768b859
Oracle Public Cloud User <opc@instance-20220413-1542.subnet04112333.vcn04112333.oraclevcn.com>
2022-07-12 20:24:19 -0400

    clean up files

--- cockdb_env.sh
+++
@@ -1,1 +0,0 @@
-export DATABASE_URL="postgresql://andywangevertz:YJgMUQ4y_V6vTkp_k0BWcw@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dnewer-krill-412&sslrootcert=$HOME/.postgresql/root.crt"
